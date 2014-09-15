0511f48765760c35d3db1633c4ace49dd40492b0
Pubudu Dissanayake <pubudu08@gmail.com>
2014-09-15 09:22:58 +0530

    Codereview: Refactored Exception Handling

--- src/main/java/org/wso2/carbon/dev/govern/extractor/ArtifactPropertyValueExtractor.java
+++ src/main/java/org/wso2/carbon/dev/govern/extractor/ArtifactPropertyValueExtractor.java
@@ -5,11 +5,19 @@ package org.wso2.carbon.dev.govern.extractor;
  * @author Pubudu Dissanayake : pubudud@wso2.com  on 01/04/2014.
  */
 
-public interface IPropertyExtractor {
+import org.wso2.carbon.dev.govern.extractor.exception.GenericArtifactException;
+
+import javax.xml.stream.XMLStreamException;
+import java.io.IOException;
+
+public interface ArtifactPropertyValueExtractor {
 	/**
-	 *   This method will extract all necessary artifact properties, which contained artifact's
+	 * This method will extract all necessary artifact properties, which contained artifact's
 	 * username and password ( will be secured using SecureVault ).
+	 *
+	 * @throws IOException
+	 * @throws XMLStreamException
 	 */
-    public void xmlExtractor();
+	public void performSuperUserXMLPropertyExtraction() throws GenericArtifactException;
 
 }
